0f4bba180a0d4692e09ed860f455fed74f735c34
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-11-19 11:14:34 -0700

    winning logic fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,6 @@
 const board = document.querySelector(".board");
 const gameInfoDisplay = document.querySelector(".game-info");
-
 const gridArray = ["", "", "", "", "", "", "", "", ""];
-
-function drawBoard() {
-  board.innerHTML = "";
-  gridArray.map((cellValue, index) => {
-    const cell = document.createElement("div");
-    cell.classList.add(`square`);
-    cell.classList.add(`square${index}`);
-    cell.addEventListener("click", handleClick);
-    if (cellValue != "") {
-      cell.textContent = cellValue;
-    }
-    board.appendChild(cell);
-  });
-  checkWinner();
-}
 
 function Game() {
   const computerPlayer = "O";
@@ -43,6 +27,21 @@ function Game() {
 }
 
 let game = new Game();
+
+function drawBoard() {
+  board.innerHTML = "";
+  gridArray.map((cellValue, index) => {
+    const cell = document.createElement("div");
+    cell.classList.add(`square`);
+    cell.classList.add(`square${index}`);
+    cell.addEventListener("click", handleClick);
+    if (cellValue != "") {
+      cell.textContent = cellValue;
+    }
+    board.appendChild(cell);
+  });
+  checkWinner();
+}
 
 function resetGame() {
   for (let i = 0; i < gridArray.length; i++) {
@@ -72,12 +71,14 @@ function checkWinner() {
     if (gridArray[0] === "") return;
     game.setWinner(gridArray[0]);
   } else if (
-    (gridArray[1] === gridArray[4] && gridArray[1] === gridArray[7]) ||
-    (gridArray[1] === gridArray[5] && gridArray[2] === gridArray[8])
+    (gridArray[1] === gridArray[4] && gridArray[1] === gridArray[7])
   ) {
     if (gridArray[1] === "") return;
     game.setWinner(gridArray[1]);
-  } else if (gridArray[2] === gridArray[4] && gridArray[2] === gridArray[6]) {
+  } else if (
+    (gridArray[2] === gridArray[4] && gridArray[2] === gridArray[6]) ||
+    (gridArray[2] === gridArray[5] && gridArray[2] === gridArray[8])
+  ) {
     if (gridArray[2] === "") return;
     game.setWinner(gridArray[2]);
   } else if (gridArray[3] === gridArray[4] && gridArray[3] === gridArray[5]) {
